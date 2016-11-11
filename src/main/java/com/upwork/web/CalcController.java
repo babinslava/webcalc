b5d6c88cf602616bb6c0afa765b42ee1e579e5bf
Slava Babin <vbabin@bsoft.biz>
2016-11-11 11:51:27 +0200

    CalcController was added.

--- src/main/java/com/upwork/web/CalcController.java
+++ src/main/java/com/upwork/web/CalcController.java
@@ -1,7 +1,14 @@
 package com.upwork.web;
 
-/**
- * Created by vbabin on 11.11.2016.
- */
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalcController {
+
+    @RequestMapping(value = "/add/{a}/{b}/{c}")
+    public Integer add(@PathVariable("a") Integer operandA, @PathVariable("b") Integer operandB, @PathVariable("c") Integer operandC){
+        return operandA+operandB+operandC;
+    }
 }
