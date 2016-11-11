1b27af0d22019698255341aee0e6cf0d0cabb499
Slava Babin <vbabin@bsoft.biz>
2016-11-11 23:43:07 +0200

    Subtract method and its tests was added to the controller. Cache key contains method name now.

--- src/main/java/com/upwork/web/CalcController.java
+++ src/main/java/com/upwork/web/CalcController.java
@@ -1,6 +1,7 @@
 package com.upwork.web;
 
 import com.upwork.dto.Result;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalcController {
 
-    @Cacheable("calc")
+    @Cacheable(value="calc",
+            key="new org.springframework.cache.interceptor.SimpleKey( #root.methodName, #operandA, #operandB, #operandC  )")
     @RequestMapping(value = "/add/{a}/{b}/{c:.+}")
     public Result add(@PathVariable("a") float operandA, @PathVariable("b") float operandB, @PathVariable("c") float operandC){
         return new Result(operandA+operandB+operandC);
+    }
+
+    @Cacheable(value="calc",
+            key="new org.springframework.cache.interceptor.SimpleKey( #root.methodName, #operandA, #operandB, #operandC  )")
+    @RequestMapping(value = "/subtract/{a}/{b}/{c:.+}")
+    public Result subtract(@PathVariable("a") float operandA, @PathVariable("b") float operandB, @PathVariable("c") float operandC){
+        return new Result(operandA-operandB-operandC);
     }
 }
--- src/test/java/com/upwork/WebCalculatorApplicationTests.java
+++ src/test/java/com/upwork/WebCalculatorApplicationTests.java
@@ -47,16 +47,17 @@ public class WebCalculatorApplicationTests {
 		b=2.3f;
 		c=3.1f;
 
-		given().
-			pathParam("a",a).
-			pathParam("b",b).
-			pathParam("c",c).
-			contentType(JSON).
-		when().
-			get("/add/{a}/{b}/{c}").
-		then().
-			assertThat().statusCode(200).
-			body("result",Matchers.equalTo(a+b+c));
+		callCalcMethodAndCheckResult("add",a,b,c,a+b+c);
+	}
+
+	@Test
+	public void subtractMethodShouldSubtractValues() {
+		float a,b,c;
+		a=1.2f;
+		b=2.3f;
+		c=3.1f;
+
+		callCalcMethodAndCheckResult("subtract",a,b,c,a-b-c);
 	}
 
 	@Test
@@ -66,20 +67,54 @@ public class WebCalculatorApplicationTests {
 		b=0.2f;
 		c=-3.1f;
 
-		given().
-				pathParam("a",a).
-				pathParam("b",b).
-				pathParam("c",c).
-				contentType(JSON).
-				when().
-				get("/add/{a}/{b}/{c}").
-				then().
-				assertThat().statusCode(200);
-
-		Ehcache cache = (Ehcache) cacheManager.getCache("calc").getNativeCache();
-		SimpleKey key = new SimpleKey(a,b,c);
-		final Element cachedResult = cache.getQuiet(key);
-		assertThat("Should find element in cache",cachedResult,notNullValue());
-		assertThat("Should be the right result",((Result) cachedResult.getObjectValue()).getResult(),Matchers.equalTo(a+b+c));
+		String methodName="add";
+		float result = a+b+c;
+		callCalcMethod(methodName,a,b,c);
+		checkResultInCache(new SimpleKey(methodName,a,b,c),result);
 	}
+
+	@Test
+	public void subtractMethodShouldUseCache() {
+		float a,b,c;
+		a=2f;
+		b=0.4f;
+		c=-1.6f;
+
+		String methodName="subtract";
+		float result = a-b-c;
+		callCalcMethod(methodName,a,b,c);
+		checkResultInCache(new SimpleKey(methodName,a,b,c),result);
+	}
+
+	private void checkResultInCache(SimpleKey cacheKey, float result){
+		Ehcache cache = (Ehcache) cacheManager.getCache("calc").getNativeCache();
+		final Element cachedResult = cache.getQuiet(cacheKey);
+		assertThat("Should find element in cache",cachedResult,notNullValue());
+		assertThat("Should be the right result",((Result) cachedResult.getObjectValue()).getResult(),Matchers.equalTo(result));
+	}
+
+	private void callCalcMethod(String methodName, float operandA, float operandB, float operandC){
+		given().
+			pathParam("a",operandA).
+			pathParam("b",operandB).
+			pathParam("c",operandC).
+			contentType(JSON).
+		when().
+			get("/"+methodName+"/{a}/{b}/{c}").
+		then().
+			assertThat().statusCode(200);
+	}
+	private void callCalcMethodAndCheckResult(String methodName, float operandA, float operandB, float operandC, float result){
+		given().
+			pathParam("a",operandA).
+			pathParam("b",operandB).
+			pathParam("c",operandC).
+			contentType(JSON).
+		when().
+			get("/"+methodName+"/{a}/{b}/{c}").
+		then().
+			assertThat().statusCode(200).
+			body("result",Matchers.equalTo(result));
+	}
+
 }
